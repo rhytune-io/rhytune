0ab76d167adc89d4990cf2453bcbae6fdef6a7ed
dimpurr <dimpur@live.com>
2024-02-27 17:03:29 +0000

    fix: tslint

--- rhytune-backend/src/app.ts
+++ rhytune-backend/src/app.ts
@@ -1,16 +1,22 @@
+// app.ts
+import express, { Application, Request, Response } from 'express';
 import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
-const swaggerDocument = YAML.load('./path/to/your/swagger.yaml');
 
+const app: Application = express();
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+// Body parsing Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", async (req: Request, res: Response): Promise<Response> => {
+    return res.status(200).send({
+        message: "Hello World!",
+    });
+});
+
+// Swagger UI setup
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-import passport from 'passport';
-import { Strategy as LocalStrategy } from 'passport-local';
-
-// 配置Passport中间件和策略
-app.use(passport.initialize());
-app.use(passport.session());
-
-passport.use(new LocalStrategy((username, password, done) => {
-    // 你的身份验证逻辑
-}));
+export default app;
--- rhytune-backend/src/index.ts
+++ rhytune-backend/src/index.ts
@@ -1,10 +1,16 @@
-import express from 'express';
-const app = express();
+// index.ts
+import app from './app';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+// MongoDB connection
+mongoose.connect(process.env.MONGODB_URI || '', { useNewUrlParser: true, useUnifiedTopology: true } as mongoose.ConnectOptions)
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.error('Could not connect to MongoDB...', err));
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
